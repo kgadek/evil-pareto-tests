8e49962eb53a59e3500f681cc40ed5365d4e165b
Konrad Gądek <kgadek@gmail.com>
2015-03-08 22:00:52 +0100

    MWC-Random: playground

--- src/Main.hs
+++ src/Main.hs
@@ -1,14 +1,34 @@
-{-# LANGUAGE TypeSynonymInstances #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE TupleSections #-}
+{-# LANGUAGE TypeSynonymInstances #-}
 
 module Main where
 
 import Control.Applicative
 import Control.Monad
+import Control.Monad.ST
+import Control.Monad.Primitive
 import Data.Monoid
+import Data.Word
 
 import Text.PrettyPrint (Doc)
 import qualified Text.PrettyPrint as PP
+
+import qualified System.Random.MWC as R
+import qualified Data.Vector as V
+
+--------------------------------------------------------------------------------
+
+test :: (V.Vector Word32) -> ST s Int
+test seed = do
+    R.initialize seed --   ST s (R.Gen (PrimState (ST s)))
+                      -- ≡ ST s (R.GenST s)
+    return 123
+
+genSeed :: IO (V.Vector Word32)
+genSeed = R.withSystemRandom aux
+  where aux (gen::R.GenST s) = R.uniformVector gen 255 :: ST s (V.Vector Word32)
+
 
 
 --------------------------------------------------------------------------------
@@ -73,5 +93,11 @@ main = do
     print $ select2
     print $ cross1
     print $ mutate1
+
+    seed <- genSeed
+    print seed
+    let x = runST $ test seed
+    print x
+
 
     putStrLn "OHAI"
